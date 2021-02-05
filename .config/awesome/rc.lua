445301d51229d7d9d86abc10a98626ba313f8fcf
ilkmin <no_email@noemail.not>
2021-02-05 18:44:31 +0500

    AwesomeWM: modular structure (errors, layouts, tags, variables)

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -18,43 +18,38 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
--- {{{ Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-    naughty.notify({ preset = naughty.config.presets.critical,
-                     title = "Oops, there were errors during startup!",
-                     text = awesome.startup_errors })
-end
+-- {{{ My modules
 
--- Handle runtime errors after startup
-do
-    local in_error = false
-    awesome.connect_signal("debug::error", function (err)
-        -- Make sure we don't go into an endless error loop
-        if in_error then return end
-        in_error = true
+-- Global namespace for modules
+mdl = {}
 
-        naughty.notify({ preset = naughty.config.presets.critical,
-                         title = "Oops, an error happened!",
-                         text = tostring(err) })
-        in_error = false
-    end)
-end
+-- Errors handling
+require("main.errors_check")
+
+-- User variables
+mdl.user_vars = require("main.user_vars")
+
+-- Custom local lib
+local main = {
+	layouts = require("main.layouts"),
+	tags    = require("main.tags")
+}
+
 -- }}}
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
--- This is used later as the default terminal and editor to run.
-terminal   = os.getenv("TERM") or "alacritty" or "xterm"
-editor     = os.getenv("EDITOR") or "micro" or "nano"
-editor_cmd = terminal .. " -e " .. editor
+-- Initial
+terminal    = mdl.user_vars.terminal
+editor      = mdl.user_vars.editor
+editor_cmd  = terminal .. " -e " .. editor
 
-betterlockscreen = "betterlockscreen -l -t \"It can only show time.\" "
-browser          = os.getenv("BROWSER") or "flatpak run com.brave.Browser"
-rofi             = "rofi -show run"
+-- Preference
+browser     = mdl.user_vars.brave or user_vars.browser
+lock_screen = mdl.user_vars.betterlockscreen
+menu        = mdl.user_vars.rofi
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
@@ -63,25 +58,9 @@ rofi             = "rofi -show run"
 -- However, you can use another modifier like Mod1, but it may interact with others.
 modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    -- awful.layout.suit.floating,
-    -- awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    -- awful.layout.suit.spiral,
-    -- awful.layout.suit.spiral.dwindle,
-    -- awful.layout.suit.max,
-    -- awful.layout.suit.max.fullscreen,
-    -- awful.layout.suit.magnifier,
-    -- awful.layout.suit.corner.nw,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
-}
+-- Layouts
+mdl.layouts = main.layouts()
+
 -- }}}
 
 -- {{{ Menu
@@ -168,59 +147,9 @@ local tasklist_buttons = gears.table.join(
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 --screen.connect_signal("property::geometry", set_wallpaper)
 
-awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    --set_wallpaper(s)
+-- Tags
+mdl.tags = main.tags()
 
-    -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
-
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist {
-        screen  = s,
-        filter  = awful.widget.taglist.filter.all,
-        buttons = taglist_buttons
-    }
-
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons
-    }
-
-    -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
-
-    -- Add widgets to the wibox
-    s.mywibox:setup {
-        layout = wibox.layout.align.horizontal,
-        { -- Left widgets
-            layout = wibox.layout.fixed.horizontal,
-            mylauncher,
-            s.mytaglist,
-            s.mypromptbox,
-        },
-        s.mytasklist, -- Middle widget
-        { -- Right widgets
-            layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
-            wibox.widget.systray(),
-            mytextclock,
-            s.mylayoutbox,
-        },
-    }
-end)
 -- }}}
 
 -- {{{ Mouse bindings
@@ -279,7 +208,7 @@ globalkeys = gears.table.join(
 
 	-- Lock screen
 	awful.key({ modkey, "Control" }, "/", function () 
-                                            awful.spawn(betterlockscreen) 
+                                            awful.spawn(lock_screen) 
 										  end,
 	              {description = "lock the screen", group = "lock"}),
 
@@ -354,7 +283,7 @@ globalkeys = gears.table.join(
               {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey }, "space", function () awful.spawn(rofi) end,
+    awful.key({ modkey }, "space", function () awful.spawn(menu) end,
               {description = "run rofi menu", group = "launcher"}),
 
     awful.key({ modkey }, "x",
